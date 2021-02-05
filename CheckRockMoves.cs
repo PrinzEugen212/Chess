c03582d2b12b18a20d4626c88ee26ea164f5ebc6
zekazak220@mail.ru <zekazak220@mail.ru>
2021-02-05 17:07:04 +0300

    исправленная проверка ходов для ладьи

---
+++ CheckRockMoves.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    abstract class CheckRockMoves
+    {
+        public static bool CheckMove(ChessPiece rock, Coordinate endCoordinate)
+        {
+            Coordinate[] Coordinates = Moves(rock);
+            foreach (var coordinateInArray in Coordinates)
+            {
+                if (coordinateInArray.ToString() == endCoordinate.ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static Coordinate[] Moves(ChessPiece rock)
+        {
+            Coordinate[] rockMoves = new Coordinate[14];
+            int length = 0;
+            int startVertical = rock.Coordinate.Vertical, startHorizontal = rock.Coordinate.Horizontal;
+            int vertical = startVertical, horizontal = startHorizontal;
+            while (vertical < 8)
+            {
+                vertical++;
+                rockMoves[length] = new Coordinate(vertical, horizontal);
+                length++;
+            }
+            vertical = startVertical;
+            while (horizontal > 1)
+            {
+                horizontal--;
+                rockMoves[length] = new Coordinate(vertical, horizontal);
+                length++;
+            }
+            horizontal = startHorizontal;
+            while (horizontal < 8)
+            {
+                horizontal++;
+                rockMoves[length] = new Coordinate(vertical, horizontal);
+                length++;
+            }
+            horizontal = startHorizontal;
+            while (vertical > 1)
+            {
+                vertical--;
+                rockMoves[length] = new Coordinate(vertical, horizontal);
+                length++;
+            }
+            return rockMoves;
+        }
+    }
+}
+
