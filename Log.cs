b08b8b6bdf2f3938eb7046d9d0a0391b5c6b2563
zekazak220@mail.ru <zekazak220@mail.ru>
2021-02-06 13:51:12 +0300

    Добавлена запись партии и её вывод в консоль

---
+++ Log.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    static class Log
+    {
+        static private string GameLog;
+        static public int Turn { get; private set; }
+        static public void Add(string move, string colorThatMadeMove, char figureType)
+        {
+            if (colorThatMadeMove == "White")
+            {
+                if(figureType != 'P')
+                {
+                    Turn++;
+                    GameLog += Turn.ToString() + ". " + figureType + move;
+                }
+                else
+                {
+                    Turn++;
+                    GameLog += Turn.ToString() + ". " + move;
+                }
+            }
+            else
+            {
+                if (figureType != 'P')
+                {
+                    GameLog += " " + figureType + move + " ";
+                }
+                else
+                {
+                    GameLog += " " + move + " ";
+                }
+            }
+        }
+        static public string GetLog()
+        {
+            return GameLog;
+        }
+        static public void ShowInConsole()
+        {
+            Console.SetCursorPosition(30, 0);
+            Console.Write(GetLog());
+            Console.SetCursorPosition(0, 10);
+        }
+    }
+}
