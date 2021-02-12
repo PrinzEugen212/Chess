f058e917b744fb1164e2a48bdd73117d452217a0
zekazak220@mail.ru <zekazak220@mail.ru>
2021-02-12 15:02:04 +0300

    Заменён тип string на StringBuilder

--- Chess/Log.cs
+++ Chess/Log.cs
@@ -6,7 +6,7 @@ namespace Chess
 {
     static class Log
     {
-        static private string GameLog;
+        static private StringBuilder GameLog = new StringBuilder();
         static public int Turn { get; private set; }
         static public void Add(string move, string colorThatMadeMove, char figureType)
         {
@@ -15,35 +15,37 @@ namespace Chess
                 if(figureType != 'P')
                 {
                     Turn++;
-                    GameLog += Turn.ToString() + ". " + figureType + move;
+                    GameLog.Append (Turn.ToString() + ". " + figureType + move);
                 }
                 else
                 {
                     Turn++;
-                    GameLog += Turn.ToString() + ". " + move;
+                    GameLog.Append (Turn.ToString() + ". " + move);
                 }
             }
             else
             {
                 if (figureType != 'P')
                 {
-                    GameLog += " " + figureType + move + " ";
+                    GameLog.Append(" " + figureType + move + " ");
                 }
                 else
                 {
-                    GameLog += " " + move + " ";
+                    GameLog.Append(" " + move + " ");
                 }
             }
         }
         static public string GetLog()
         {
-            return GameLog;
-        }
-        static public void ShowInConsole()
-        {
-            Console.SetCursorPosition(30, 0);
-            Console.Write(GetLog());
-            Console.SetCursorPosition(0, 10);
+            if (GameLog == null)
+            {
+                return " ";
+            }
+            else
+            {
+                return GameLog.ToString();
+            }
+            
         }
     }
 }
